0e6aab158ad70b36ab579032fbf08d598f67ee6e
Pay2Utku <pay2utku@gmail.com>
2024-06-23 11:09:38 +0300

    basic player movement with joystick

--- Assets/_Game/Scripts/Player.cs
+++ Assets/_Game/Scripts/Player.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] private FloatingJoystick _joystick;
+
+    [SerializeField] private float _moveSpeed;
     private void Update()
     {
-
+        Vector3 moveDirection = new Vector3(_joystick.Horizontal, 0, _joystick.Vertical);
+        transform.Translate(moveDirection * _moveSpeed * Time.deltaTime);
     }
 }
